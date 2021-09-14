51732d165a827bf4a6fdbbc141d755a864f3c098
Woojin <sam4woojin@naver.com>
2021-09-14 16:04:44 +0900

    Show map Online & Offline

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import L from "leaflet";
+import localforage from "localforage";
+import "leaflet-offline";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    const map = L.map("map-id");
+    const offlineLayer = L.tileLayer.offline(
+      "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      localforage,
+      {
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        subdomains: "abc",
+        minZoom: 13,
+        maxZoom: 19,
+        crossOrigin: true,
+      }
+    );
+    offlineLayer.addTo(map);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div id="map-id">
+        <MapContainer
+          center={[37.47386563818747, 127.14299349434039]}
+          zoom={13}
+          maxZoom={16}
+          id="map"
         >
-          Learn React
-        </a>
-      </header>
+          <TileLayer
+            attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            maxZoom={16}
+            tileSize={512}
+            zoomOffset={-1}
+          />
+          <Marker position={[37.47386563818747, 127.14299349434039]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
